09520db4587feb1d5f92e729000f1ef432700b60
Saffottiglia <103639413+Saffottiglia@users.noreply.github.com>
2024-01-05 10:28:32 +0100

    Add files via upload

---
+++ src/Dadi.cpp
@@ -0,0 +1,52 @@
+// Autore: Chellin Davide
+// ------------------------------
+
+
+#include <cstdlib>
+#include "Dadi.h"
+#include <time.h>
+
+
+
+	Dadi::Dadi(){
+		
+	numeroFacce=6;
+	conteggioTiri=0;
+	
+	}
+	
+	
+	Dadi::Dadi(int numeroFacceDado){
+		
+	numeroFacce=numeroFacceDado;
+	conteggioTiri=0;
+
+	}
+	
+	int Dadi::tiraDadi(int numeroDadi){
+		
+		conteggioTiri=conteggioTiri + 1;
+		
+		sommaDadiLanciati=0;
+				
+
+		for(int i=0; i<numeroDadi; i++){
+			
+			std::srand(time(0));
+			int d = std::rand() % (numeroFacce-1) + 1;
+			
+			sommaDadiLanciati = sommaDadiLanciati + d;
+								
+			}
+			
+		
+		return sommaDadiLanciati;
+		
+		}
+	
+	int Dadi::getNumeroTiri(){
+		
+		return conteggioTiri;
+		
+		}
+
