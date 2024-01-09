957eda173938979b5041d568021b915f87b82bb9
Saffottiglia <103639413+Saffottiglia@users.noreply.github.com>
2024-01-09 21:51:44 +0100

    Add files via upload

---
+++ include/Gioco.h
@@ -0,0 +1,16 @@
+class Gioco{
+
+    Gioco(flag da argv gicoatore umani e non);
+
+    Gioco(flag da argv giocatore non umani);
+
+    aggiungiGiocatori(Giocarore[] giocatori);
+
+    turnoGiocatoreUmano();
+
+    turnoGiocatoreNonUmano();
+    
+    leggiInputGiocatore();
+
+
+}
