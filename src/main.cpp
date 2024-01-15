68803cc7cf1046c0bbd0386576fbb4c27bd5acc0
MarvinTheMoodLifter <marvin.themoodlifter@proton.me>
2024-01-15 15:29:41 +0100

    risolto problema main

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,18 @@ int main(int argc, char **argv) {
   // Se argc == computer allora la partita è 4 computer
   // Se argc == human allora la partita è 1 giocatore e 3 computer
   // Inizializzo il tipo di partita di default a computer
-  std::string tipoPartita = "computer";
-  // Se argc == 2 allora leggo il tipo di partita da argv[1]
-  if (argc == 2) {
-    tipoPartita = argv[1];
+  std::string tipoPartita;
+  if (argc != 2) {
+    std::cout << "Tipo di partita non valido" << std::endl;
+    std::cout << "Uso: ./main [human|computer]\n" << std::endl;
+    return 0;
   }
+  tipoPartita = argv[1];
   if (tipoPartita == "human" | tipoPartita == "computer") {
     std::cout << "Partita " << tipoPartita << std::endl;
   } else {
     std::cout << "Tipo di partita non valido" << std::endl;
-    std::cout << "Uso: ./main [human|computer]" << std::endl;
+    std::cout << "Uso: ./main [human|computer]\n" << std::endl;
     return 0;
   }
 
